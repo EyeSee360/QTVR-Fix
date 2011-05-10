8a285be46756175097ef7db208ba646bf11f8bd7
mjr <mjr@c99936ce-96fc-0310-9d12-a7d7ac1979b7>
2011-05-10 23:34:50 +0000

    More robust parsing code for samples and chunks.
    git-svn-id: svn+ssh://svn.eyesee360.com/tools/qtvrfix@12144 c99936ce-96fc-0310-9d12-a7d7ac1979b7

--- qtvrfix/main.c
+++ qtvrfix/main.c
@@ -37,6 +37,47 @@ typedef struct _Box_hdlr {
     char       name[];
 } Box_hdlr;
 
+// Sample-to-chunk box
+typedef struct _Box_stsc_entry {
+    u_int32_t first_chunk;
+    u_int32_t samples_per_chunk;
+    u_int32_t sample_desc_index;
+} Box_stsc_entry;
+
+typedef struct _box_stsc {
+    FullBox    box;
+    u_int32_t entry_count;
+    Box_stsc_entry entry[];
+} Box_stsc;
+
+u_int32_t stsc_sample_to_chunk(Box_stsc *stsc, u_int32_t sampleIndex)
+{
+    u_int32_t entryIndex = 0;
+    u_int32_t entryCount = ntohl(stsc->entry_count);
+    u_int32_t chunk = 0;
+    
+    do {
+        Box_stsc_entry *thisEntry = &stsc->entry[entryIndex];
+        Box_stsc_entry *nextEntry = &stsc->entry[entryIndex+1];
+        u_int32_t firstChunk = ntohl(thisEntry->first_chunk);
+        u_int32_t samplesPerChunk = ntohl(thisEntry->samples_per_chunk);
+        
+        if (entryIndex < entryCount - 1) {
+            u_int32_t lastChunk = ntohl(nextEntry->first_chunk);
+            
+            if (samplesPerChunk * (lastChunk - firstChunk) > sampleIndex) {
+                // not in this entry
+                continue;
+            }
+        }
+        
+        // 1-based index
+        chunk = 1 + (sampleIndex - firstChunk) / samplesPerChunk;
+    } while (++entryIndex < entryCount);
+    
+    return chunk;
+}
+
 // Sample size box
 typedef struct _Box_stsz {
     FullBox    box;
@@ -45,12 +86,23 @@ typedef struct _Box_stsz {
     u_int32_t  entry_size[];
 } Box_stsz;
 
+
 // Chunk offset box
 typedef struct _Box_stco {
     FullBox    box;
     u_int32_t  entry_count;
     u_int32_t  chunk_offset[];
 } Box_stco;
+
+u_int32_t stco_chunk_offset(Box_stco *stco, u_int32_t chunkIndex)
+{
+    u_int32_t offset = 0;
+    if (chunkIndex <= ntohl(stco->entry_count)) {
+        offset = ntohl(stco->chunk_offset[chunkIndex-1]);
+    }
+    
+    return offset;
+}
 
 
 // Atom container header
@@ -76,36 +128,8 @@ typedef struct _AtomHeader {
     u_int8_t   contents[];
 } AtomHeader;
 
-typedef struct __attribute__((packed)) _QTVRPanoSampleAtom { 
-    u_int16_t  majorVersion; 
-    u_int16_t  minorVersion; 
-    u_int32_t  imageRefTrackIndex; 
-    u_int32_t  hotSpotRefTrackIndex; 
-    
-    /* NOTE:  The following group of fields are actually 32-bit floats. 
-              For ease in byte-swapping, they have been typed as u_int32_t instead. */
-    u_int32_t  minPan;
-    u_int32_t  maxPan; 
-    u_int32_t  minTilt; 
-    u_int32_t  maxTilt; 
-    u_int32_t  minFieldOfView; 
-    u_int32_t  maxFieldOfView; 
-    u_int32_t  defaultPan; 
-    u_int32_t  defaultTilt; 
-    u_int32_t  defaultFieldOfView; 
-    
-    u_int32_t  imageSizeX; 
-    u_int32_t  imageSizeY; 
-    u_int16_t  imageNumFramesX; 
-    u_int16_t  imageNumFramesY; 
-    u_int32_t  hotSpotSizeX; 
-    u_int32_t  hotSpotSizeY; 
-    u_int16_t  hotSpotNumFramesX; 
-    u_int16_t  hotSpotNumFramesY; 
-    u_int32_t  flags;
-    u_int32_t  panoType;
-    u_int32_t  reserved;
-} QTVRPanoSampleAtom;
+
+#pragma mark Box Containers
 
 // Stucture to define a bound container in memory
 typedef struct _Container {
@@ -115,12 +139,6 @@ typedef struct _Container {
     void *     childAtomData;
     void *     boxExtent;
 } Container;
-
-
-// List of box types known to be box containers
-const u_int32_t cContainerTypes[] = { 'moov', 'trak', 'edts', 'mdia', 'minf', 'dinf', 'stbl', 'mvex', 'moof', 'traf', 'mfra', 'udta', 'meta', 'ipro', 'sinf' };
-const int cContainerTypesCount = sizeof(cContainerTypes) / sizeof(u_int32_t);
-
 
 Container init_container(void *data, void *extent)
 {
@@ -164,7 +182,6 @@ Container init_container_atom_container(AtomContainer *data)
     return container;
 }
 
-
 static int sIndentLevel = 0;
 void print_box(const Container *boxContainer)
 {
@@ -175,6 +192,10 @@ void print_box(const Container *boxContainer)
 
 int is_type_container(u_int32_t type)
 {
+    // List of box types known to be box containers
+    const u_int32_t cContainerTypes[] = { 'moov', 'trak', 'edts', 'mdia', 'minf', 'dinf', 'stbl', 'mvex', 'moof', 'traf', 'mfra', 'udta', 'meta', 'ipro', 'sinf' };
+    const int cContainerTypesCount = sizeof(cContainerTypes) / sizeof(u_int32_t);
+
     for (int i = 0; i < cContainerTypesCount; i++) {
         if (cContainerTypes[i] == type) {
             return 1;
@@ -235,6 +256,40 @@ void print_container_r(const Container *boxContainer)
     });
 }
 
+
+#pragma mark QTVR Types
+
+typedef struct __attribute__((packed)) _QTVRPanoSampleAtom { 
+    u_int16_t  majorVersion; 
+    u_int16_t  minorVersion; 
+    u_int32_t  imageRefTrackIndex; 
+    u_int32_t  hotSpotRefTrackIndex; 
+    
+    /* NOTE:  The following group of fields are actually 32-bit floats. 
+     For ease in byte-swapping, they have been typed as u_int32_t instead. */
+    u_int32_t  minPan;
+    u_int32_t  maxPan; 
+    u_int32_t  minTilt; 
+    u_int32_t  maxTilt; 
+    u_int32_t  minFieldOfView; 
+    u_int32_t  maxFieldOfView; 
+    u_int32_t  defaultPan; 
+    u_int32_t  defaultTilt; 
+    u_int32_t  defaultFieldOfView; 
+    
+    u_int32_t  imageSizeX; 
+    u_int32_t  imageSizeY; 
+    u_int16_t  imageNumFramesX; 
+    u_int16_t  imageNumFramesY; 
+    u_int32_t  hotSpotSizeX; 
+    u_int32_t  hotSpotSizeY; 
+    u_int16_t  hotSpotNumFramesX; 
+    u_int16_t  hotSpotNumFramesY; 
+    u_int32_t  flags;
+    u_int32_t  panoType;
+    u_int32_t  reserved;
+} QTVRPanoSampleAtom;
+
 void swap_pano_sample(QTVRPanoSampleAtom *pdatIn, QTVRPanoSampleAtom *pdatOut)
 {
     pdatOut->majorVersion = ntohs(pdatIn->majorVersion); 
@@ -265,8 +320,10 @@ void swap_pano_sample(QTVRPanoSampleAtom *pdatIn, QTVRPanoSampleAtom *pdatOut)
     return;
 }
 
-void update_pano_sample(void *panoSample, u_int32_t size)
+int update_pano_sample(void *panoSample, u_int32_t size)
 {
+    int didChange = 0;
+    
     AtomContainer *atomContainer = (AtomContainer *)panoSample;
     Container panoSampleContainer = init_container_atom_container(atomContainer);
     
@@ -279,11 +336,15 @@ void update_pano_sample(void *panoSample, u_int32_t size)
     // This is the actual fix
     if (pdatNative.hotSpotSizeX == 0) {
         // no hotspots. Num frames should be 0
+        didChange = !!(pdatNative.hotSpotNumFramesX + pdatNative.hotSpotNumFramesY);
+
         pdatNative.hotSpotNumFramesX = 0;
         pdatNative.hotSpotNumFramesY = 0;
+
+        swap_pano_sample(&pdatNative, pdat);
     }
     
-    swap_pano_sample(&pdatNative, pdat);
+    return didChange;
 }
 
 int qtvrfix (const char *moviePath)
@@ -323,49 +384,44 @@ int qtvrfix (const char *moviePath)
                 
                 Container minfBox = find_single_box(&mdiaBox, 'minf');
                 Container stblBox = find_single_box(&minfBox, 'stbl');
+                Container stscBox = find_single_box(&stblBox, 'stsc');
+                Container stcoBox = find_single_box(&stblBox, 'stco');
+                Container stszBox = find_single_box(&stblBox, 'stsz');
                 
-                __block u_int32_t panoSampleSize = 0;
-                __block u_int32_t panoSampleOffset = 0;
-                
-                enumerate_boxes(&stblBox, 0, ^(Container sampleBox, int *stop) {
-                    switch (sampleBox.boxHeader.type) {
-                        case 'stsz': {
-                            Box_stsz *stsz = (Box_stsz *) sampleBox.boxStart;
-                            u_int32_t sampleCount = ntohl(stsz->sample_count);
-                            u_int32_t sampleSize = ntohl(stsz->sample_size);
-                            
-                            if (sampleCount > 1) {
-                                fprintf(stderr, "found %d samples in 'pano' table; expected 1", sampleCount);
-                                assert(sampleCount == 1);
-                            }
-                            
-                            if (sampleSize == 0) {
-                                panoSampleSize = ntohl(stsz->entry_size[0]);
-                            } else {
-                                panoSampleSize = sampleSize;
-                            }
-                        }
-                            break;
-                        case 'stco': {
-                            Box_stco *stco = (Box_stco *) sampleBox.boxStart;
-                            u_int32_t entryCount = ntohl(stco->entry_count);
-                            
-                            if (entryCount > 1) {
-                                fprintf(stderr, "found %d chunks in 'stco' table; expected 1", stco->entry_count);
-                                assert(stco->entry_count == 1);
-                            }
-                            panoSampleOffset = ntohl(stco->chunk_offset[0]);
-                        }
-                            break;
-                        default:
-                            break;
-                    }
-                });
-                
-                printf("Pano sample of size %d bytes at offset %d\n", panoSampleSize, panoSampleOffset);
+                Box_stsc *stsc = (Box_stsc *) stscBox.boxStart;
+                Box_stco *stco = (Box_stco *) stcoBox.boxStart;
+                Box_stsz *stsz = (Box_stsz *) stszBox.boxStart;
 
-                void *panoSample = movieData + panoSampleOffset;
-                update_pano_sample(panoSample, panoSampleSize);
+                u_int32_t sampleCount = ntohl(stsz->sample_count);
+                u_int32_t sampleSize = ntohl(stsz->sample_size);
+                u_int32_t lastPanoSampleChunk = 0;
+                u_int32_t cumuChunkOffset = 0;
+                int updatedSamples = 0;
+
+                for (int sampleIndex = 1; sampleIndex <= sampleCount; sampleIndex++) {
+                    u_int32_t panoSampleSize = sampleSize;
+                    u_int32_t panoSampleOffset = 0;
+                    u_int32_t panoSampleChunk = stsc_sample_to_chunk(stsc, sampleIndex);
+                    
+                    if (sampleSize == 0) {
+                        panoSampleSize = ntohl(stsz->entry_size[sampleIndex-1]);
+                    }
+
+                    if (panoSampleChunk != lastPanoSampleChunk) {
+                        cumuChunkOffset = 0;
+                        lastPanoSampleChunk = panoSampleChunk;
+                    }
+                    
+                    panoSampleOffset = stco_chunk_offset(stco, panoSampleChunk) + cumuChunkOffset;
+                    cumuChunkOffset += panoSampleSize;
+                    
+                    void *panoSample = movieData + panoSampleOffset;
+                    updatedSamples += update_pano_sample(panoSample, panoSampleSize);
+                }
+                
+                if (updatedSamples > 0) {
+                    printf("Updated file %s\n", moviePath);
+                }
             }
             
         });
